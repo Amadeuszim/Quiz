d2891264ee802e58bd016f9447dd75699f6600f6
AmaGod <43050667+Amadeuszim@users.noreply.github.com>
2020-03-03 10:45:29 -0300

    Add files via upload

---
+++ quizz/quizz/Form1.Designer.vb
@@ -0,0 +1,501 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnfalso1 = New System.Windows.Forms.Button()
+        Me.btnver1 = New System.Windows.Forms.Button()
+        Me.btniniciar = New System.Windows.Forms.Button()
+        Me.btnresposta = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnver2 = New System.Windows.Forms.Button()
+        Me.btnver3 = New System.Windows.Forms.Button()
+        Me.btnver4 = New System.Windows.Forms.Button()
+        Me.btnver5 = New System.Windows.Forms.Button()
+        Me.btnfalso2 = New System.Windows.Forms.Button()
+        Me.btnfalso3 = New System.Windows.Forms.Button()
+        Me.btnfalso4 = New System.Windows.Forms.Button()
+        Me.btnfalso5 = New System.Windows.Forms.Button()
+        Me.btnfalso10 = New System.Windows.Forms.Button()
+        Me.btnfalso9 = New System.Windows.Forms.Button()
+        Me.btnfalso8 = New System.Windows.Forms.Button()
+        Me.btnfalso7 = New System.Windows.Forms.Button()
+        Me.btnver10 = New System.Windows.Forms.Button()
+        Me.btnver9 = New System.Windows.Forms.Button()
+        Me.btnver8 = New System.Windows.Forms.Button()
+        Me.btnver7 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnver6 = New System.Windows.Forms.Button()
+        Me.btnfalso6 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.lblcerto = New System.Windows.Forms.Label()
+        Me.lblerrado = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(165, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "1) O nome do Sub-zero é Hanzo?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(175, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "2) Rainden é o Deus da escuridão?"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 113)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "3) Liu kang é um monge?"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 164)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(155, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "4) Scorpion matou Shao Kahn?"
+        '
+        'btnfalso1
+        '
+        Me.btnfalso1.Enabled = False
+        Me.btnfalso1.Location = New System.Drawing.Point(96, 27)
+        Me.btnfalso1.Name = "btnfalso1"
+        Me.btnfalso1.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso1.TabIndex = 4
+        Me.btnfalso1.Text = "Falso"
+        Me.btnfalso1.UseVisualStyleBackColor = True
+        '
+        'btnver1
+        '
+        Me.btnver1.Enabled = False
+        Me.btnver1.Location = New System.Drawing.Point(15, 27)
+        Me.btnver1.Name = "btnver1"
+        Me.btnver1.Size = New System.Drawing.Size(75, 23)
+        Me.btnver1.TabIndex = 5
+        Me.btnver1.Text = "Verdadeiro"
+        Me.btnver1.UseVisualStyleBackColor = True
+        '
+        'btniniciar
+        '
+        Me.btniniciar.Location = New System.Drawing.Point(258, 12)
+        Me.btniniciar.Name = "btniniciar"
+        Me.btniniciar.Size = New System.Drawing.Size(117, 120)
+        Me.btniniciar.TabIndex = 14
+        Me.btniniciar.Text = "Iniciar"
+        Me.btniniciar.UseVisualStyleBackColor = True
+        '
+        'btnresposta
+        '
+        Me.btnresposta.Enabled = False
+        Me.btnresposta.Location = New System.Drawing.Point(258, 159)
+        Me.btnresposta.Name = "btnresposta"
+        Me.btnresposta.Size = New System.Drawing.Size(117, 44)
+        Me.btnresposta.TabIndex = 15
+        Me.btnresposta.Text = "Respostas"
+        Me.btnresposta.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 217)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(123, 13)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "5) Noob era o Sub-zero?"
+        '
+        'btnver2
+        '
+        Me.btnver2.Enabled = False
+        Me.btnver2.Location = New System.Drawing.Point(15, 78)
+        Me.btnver2.Name = "btnver2"
+        Me.btnver2.Size = New System.Drawing.Size(75, 23)
+        Me.btnver2.TabIndex = 17
+        Me.btnver2.Text = "Verdadeiro"
+        Me.btnver2.UseVisualStyleBackColor = True
+        '
+        'btnver3
+        '
+        Me.btnver3.Enabled = False
+        Me.btnver3.Location = New System.Drawing.Point(15, 129)
+        Me.btnver3.Name = "btnver3"
+        Me.btnver3.Size = New System.Drawing.Size(75, 23)
+        Me.btnver3.TabIndex = 18
+        Me.btnver3.Text = "Verdadeiro"
+        Me.btnver3.UseVisualStyleBackColor = True
+        '
+        'btnver4
+        '
+        Me.btnver4.Enabled = False
+        Me.btnver4.Location = New System.Drawing.Point(15, 180)
+        Me.btnver4.Name = "btnver4"
+        Me.btnver4.Size = New System.Drawing.Size(75, 23)
+        Me.btnver4.TabIndex = 19
+        Me.btnver4.Text = "Verdadeiro"
+        Me.btnver4.UseVisualStyleBackColor = True
+        '
+        'btnver5
+        '
+        Me.btnver5.Enabled = False
+        Me.btnver5.Location = New System.Drawing.Point(15, 233)
+        Me.btnver5.Name = "btnver5"
+        Me.btnver5.Size = New System.Drawing.Size(75, 23)
+        Me.btnver5.TabIndex = 20
+        Me.btnver5.Text = "Verdadeiro"
+        Me.btnver5.UseVisualStyleBackColor = True
+        '
+        'btnfalso2
+        '
+        Me.btnfalso2.Enabled = False
+        Me.btnfalso2.Location = New System.Drawing.Point(96, 78)
+        Me.btnfalso2.Name = "btnfalso2"
+        Me.btnfalso2.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso2.TabIndex = 21
+        Me.btnfalso2.Text = "Falso"
+        Me.btnfalso2.UseVisualStyleBackColor = True
+        '
+        'btnfalso3
+        '
+        Me.btnfalso3.Enabled = False
+        Me.btnfalso3.Location = New System.Drawing.Point(96, 129)
+        Me.btnfalso3.Name = "btnfalso3"
+        Me.btnfalso3.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso3.TabIndex = 22
+        Me.btnfalso3.Text = "Falso"
+        Me.btnfalso3.UseVisualStyleBackColor = True
+        '
+        'btnfalso4
+        '
+        Me.btnfalso4.Enabled = False
+        Me.btnfalso4.Location = New System.Drawing.Point(96, 180)
+        Me.btnfalso4.Name = "btnfalso4"
+        Me.btnfalso4.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso4.TabIndex = 23
+        Me.btnfalso4.Text = "Falso"
+        Me.btnfalso4.UseVisualStyleBackColor = True
+        '
+        'btnfalso5
+        '
+        Me.btnfalso5.Enabled = False
+        Me.btnfalso5.Location = New System.Drawing.Point(96, 233)
+        Me.btnfalso5.Name = "btnfalso5"
+        Me.btnfalso5.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso5.TabIndex = 24
+        Me.btnfalso5.Text = "Falso"
+        Me.btnfalso5.UseVisualStyleBackColor = True
+        '
+        'btnfalso10
+        '
+        Me.btnfalso10.Enabled = False
+        Me.btnfalso10.Location = New System.Drawing.Point(96, 494)
+        Me.btnfalso10.Name = "btnfalso10"
+        Me.btnfalso10.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso10.TabIndex = 39
+        Me.btnfalso10.Text = "Falso"
+        Me.btnfalso10.UseVisualStyleBackColor = True
+        '
+        'btnfalso9
+        '
+        Me.btnfalso9.Enabled = False
+        Me.btnfalso9.Location = New System.Drawing.Point(96, 441)
+        Me.btnfalso9.Name = "btnfalso9"
+        Me.btnfalso9.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso9.TabIndex = 38
+        Me.btnfalso9.Text = "Falso"
+        Me.btnfalso9.UseVisualStyleBackColor = True
+        '
+        'btnfalso8
+        '
+        Me.btnfalso8.Enabled = False
+        Me.btnfalso8.Location = New System.Drawing.Point(96, 390)
+        Me.btnfalso8.Name = "btnfalso8"
+        Me.btnfalso8.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso8.TabIndex = 37
+        Me.btnfalso8.Text = "Falso"
+        Me.btnfalso8.UseVisualStyleBackColor = True
+        '
+        'btnfalso7
+        '
+        Me.btnfalso7.Enabled = False
+        Me.btnfalso7.Location = New System.Drawing.Point(96, 339)
+        Me.btnfalso7.Name = "btnfalso7"
+        Me.btnfalso7.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso7.TabIndex = 36
+        Me.btnfalso7.Text = "Falso"
+        Me.btnfalso7.UseVisualStyleBackColor = True
+        '
+        'btnver10
+        '
+        Me.btnver10.Enabled = False
+        Me.btnver10.Location = New System.Drawing.Point(15, 494)
+        Me.btnver10.Name = "btnver10"
+        Me.btnver10.Size = New System.Drawing.Size(75, 23)
+        Me.btnver10.TabIndex = 35
+        Me.btnver10.Text = "Verdadeiro"
+        Me.btnver10.UseVisualStyleBackColor = True
+        '
+        'btnver9
+        '
+        Me.btnver9.Enabled = False
+        Me.btnver9.Location = New System.Drawing.Point(15, 441)
+        Me.btnver9.Name = "btnver9"
+        Me.btnver9.Size = New System.Drawing.Size(75, 23)
+        Me.btnver9.TabIndex = 34
+        Me.btnver9.Text = "Verdadeiro"
+        Me.btnver9.UseVisualStyleBackColor = True
+        '
+        'btnver8
+        '
+        Me.btnver8.Enabled = False
+        Me.btnver8.Location = New System.Drawing.Point(15, 390)
+        Me.btnver8.Name = "btnver8"
+        Me.btnver8.Size = New System.Drawing.Size(75, 23)
+        Me.btnver8.TabIndex = 33
+        Me.btnver8.Text = "Verdadeiro"
+        Me.btnver8.UseVisualStyleBackColor = True
+        '
+        'btnver7
+        '
+        Me.btnver7.Enabled = False
+        Me.btnver7.Location = New System.Drawing.Point(15, 339)
+        Me.btnver7.Name = "btnver7"
+        Me.btnver7.Size = New System.Drawing.Size(75, 23)
+        Me.btnver7.TabIndex = 32
+        Me.btnver7.Text = "Verdadeiro"
+        Me.btnver7.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 478)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(137, 13)
+        Me.Label6.TabIndex = 31
+        Me.Label6.Text = "10) Liu Kang matou raiden?"
+        '
+        'btnver6
+        '
+        Me.btnver6.Enabled = False
+        Me.btnver6.Location = New System.Drawing.Point(15, 288)
+        Me.btnver6.Name = "btnver6"
+        Me.btnver6.Size = New System.Drawing.Size(75, 23)
+        Me.btnver6.TabIndex = 30
+        Me.btnver6.Text = "Verdadeiro"
+        Me.btnver6.UseVisualStyleBackColor = True
+        '
+        'btnfalso6
+        '
+        Me.btnfalso6.Enabled = False
+        Me.btnfalso6.Location = New System.Drawing.Point(96, 288)
+        Me.btnfalso6.Name = "btnfalso6"
+        Me.btnfalso6.Size = New System.Drawing.Size(75, 23)
+        Me.btnfalso6.TabIndex = 29
+        Me.btnfalso6.Text = "Falso"
+        Me.btnfalso6.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 425)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(110, 13)
+        Me.Label7.TabIndex = 28
+        Me.Label7.Text = "9) Sonia era General?"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 374)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(111, 13)
+        Me.Label8.TabIndex = 27
+        Me.Label8.Text = "8) Kano era um herói?"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 323)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(189, 13)
+        Me.Label9.TabIndex = 26
+        Me.Label9.Text = "7) O nome do pai da Cassie é Jhonny?"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 270)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(126, 13)
+        Me.Label10.TabIndex = 25
+        Me.Label10.Text = "6) Jax perdeu os braços?"
+        '
+        'lblcerto
+        '
+        Me.lblcerto.AutoSize = True
+        Me.lblcerto.Location = New System.Drawing.Point(362, 243)
+        Me.lblcerto.Name = "lblcerto"
+        Me.lblcerto.Size = New System.Drawing.Size(13, 13)
+        Me.lblcerto.TabIndex = 40
+        Me.lblcerto.Text = "0"
+        '
+        'lblerrado
+        '
+        Me.lblerrado.AutoSize = True
+        Me.lblerrado.Location = New System.Drawing.Point(362, 288)
+        Me.lblerrado.Name = "lblerrado"
+        Me.lblerrado.Size = New System.Drawing.Size(13, 13)
+        Me.lblerrado.TabIndex = 41
+        Me.lblerrado.Text = "0"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(255, 243)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(87, 13)
+        Me.Label11.TabIndex = 42
+        Me.Label11.Text = "Total de acertos:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(255, 288)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(75, 13)
+        Me.Label12.TabIndex = 43
+        Me.Label12.Text = "Total de erros:"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 5000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(413, 548)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.lblerrado)
+        Me.Controls.Add(Me.lblcerto)
+        Me.Controls.Add(Me.btnfalso10)
+        Me.Controls.Add(Me.btnfalso9)
+        Me.Controls.Add(Me.btnfalso8)
+        Me.Controls.Add(Me.btnfalso7)
+        Me.Controls.Add(Me.btnver10)
+        Me.Controls.Add(Me.btnver9)
+        Me.Controls.Add(Me.btnver8)
+        Me.Controls.Add(Me.btnver7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btnver6)
+        Me.Controls.Add(Me.btnfalso6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.btnfalso5)
+        Me.Controls.Add(Me.btnfalso4)
+        Me.Controls.Add(Me.btnfalso3)
+        Me.Controls.Add(Me.btnfalso2)
+        Me.Controls.Add(Me.btnver5)
+        Me.Controls.Add(Me.btnver4)
+        Me.Controls.Add(Me.btnver3)
+        Me.Controls.Add(Me.btnver2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnresposta)
+        Me.Controls.Add(Me.btniniciar)
+        Me.Controls.Add(Me.btnver1)
+        Me.Controls.Add(Me.btnfalso1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Quiz"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnfalso1 As System.Windows.Forms.Button
+    Friend WithEvents btnver1 As System.Windows.Forms.Button
+    Friend WithEvents btniniciar As System.Windows.Forms.Button
+    Friend WithEvents btnresposta As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents btnver2 As System.Windows.Forms.Button
+    Friend WithEvents btnver3 As System.Windows.Forms.Button
+    Friend WithEvents btnver4 As System.Windows.Forms.Button
+    Friend WithEvents btnver5 As System.Windows.Forms.Button
+    Friend WithEvents btnfalso2 As System.Windows.Forms.Button
+    Friend WithEvents btnfalso3 As System.Windows.Forms.Button
+    Friend WithEvents btnfalso4 As System.Windows.Forms.Button
+    Friend WithEvents btnfalso5 As System.Windows.Forms.Button
+    Friend WithEvents btnfalso10 As System.Windows.Forms.Button
+    Friend WithEvents btnfalso9 As System.Windows.Forms.Button
+    Friend WithEvents btnfalso8 As System.Windows.Forms.Button
+    Friend WithEvents btnfalso7 As System.Windows.Forms.Button
+    Friend WithEvents btnver10 As System.Windows.Forms.Button
+    Friend WithEvents btnver9 As System.Windows.Forms.Button
+    Friend WithEvents btnver8 As System.Windows.Forms.Button
+    Friend WithEvents btnver7 As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents btnver6 As System.Windows.Forms.Button
+    Friend WithEvents btnfalso6 As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents lblcerto As System.Windows.Forms.Label
+    Friend WithEvents lblerrado As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ quizz/quizz/Form1.vb
@@ -0,0 +1,199 @@
+﻿Public Class Form1
+
+    Public certo As Integer
+    Public errado As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub btnver1_Click(sender As Object, e As EventArgs) Handles btnver1.Click
+        btnver1.Enabled = False
+        btnfalso1.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnfalso1_Click(sender As Object, e As EventArgs) Handles btnfalso1.Click
+        btnver1.Enabled = False
+        btnfalso1.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btnver2_Click(sender As Object, e As EventArgs) Handles btnver2.Click
+        btnver2.Enabled = False
+        btnfalso2.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnfalso2_Click(sender As Object, e As EventArgs) Handles btnfalso2.Click
+        btnver2.Enabled = False
+        btnfalso2.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btnver3_Click(sender As Object, e As EventArgs) Handles btnver3.Click
+        btnver3.Enabled = False
+        btnfalso3.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btnfalso3_Click(sender As Object, e As EventArgs) Handles btnfalso3.Click
+        btnver3.Enabled = False
+        btnfalso3.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnver4_Click(sender As Object, e As EventArgs) Handles btnver4.Click
+        btnver4.Enabled = False
+        btnfalso4.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnfalso4_Click(sender As Object, e As EventArgs) Handles btnfalso4.Click
+        btnver4.Enabled = False
+        btnfalso4.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btnver5_Click(sender As Object, e As EventArgs) Handles btnver5.Click
+        btnver5.Enabled = False
+        btnfalso5.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btnfalso5_Click(sender As Object, e As EventArgs) Handles btnfalso5.Click
+        btnver5.Enabled = False
+        btnfalso5.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnver6_Click(sender As Object, e As EventArgs) Handles btnver6.Click
+        btnver6.Enabled = False
+        btnfalso6.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btnfalso6_Click(sender As Object, e As EventArgs) Handles btnfalso6.Click
+        btnver6.Enabled = False
+        btnfalso6.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnver7_Click(sender As Object, e As EventArgs) Handles btnver7.Click
+        btnver7.Enabled = False
+        btnfalso7.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btnfalso7_Click(sender As Object, e As EventArgs) Handles btnfalso7.Click
+        btnver7.Enabled = False
+        btnfalso7.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnver8_Click(sender As Object, e As EventArgs) Handles btnver8.Click
+        btnver8.Enabled = False
+        btnfalso8.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnfalso8_Click(sender As Object, e As EventArgs) Handles btnfalso8.Click
+        btnver8.Enabled = False
+        btnfalso8.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btnver9_Click(sender As Object, e As EventArgs) Handles btnver9.Click
+        btnver9.Enabled = False
+        btnfalso9.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btnfalso9_Click(sender As Object, e As EventArgs) Handles btnfalso9.Click
+        btnver9.Enabled = False
+        btnfalso9.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnver10_Click(sender As Object, e As EventArgs) Handles btnver10.Click
+        btnver10.Enabled = False
+        btnfalso10.Enabled = False
+        errado = errado + 1
+    End Sub
+
+    Private Sub btnfalso10_Click(sender As Object, e As EventArgs) Handles btnfalso10.Click
+        btnver10.Enabled = False
+        btnfalso10.Enabled = False
+        certo = certo + 1
+    End Sub
+
+    Private Sub btniniciar_Click(sender As Object, e As EventArgs) Handles btniniciar.Click
+        btnfalso1.Enabled = True
+        btnfalso2.Enabled = True
+        btnfalso3.Enabled = True
+        btnfalso4.Enabled = True
+        btnfalso5.Enabled = True
+        btnfalso6.Enabled = True
+        btnfalso7.Enabled = True
+        btnfalso8.Enabled = True
+        btnfalso9.Enabled = True
+        btnfalso10.Enabled = True
+        btnver1.Enabled = True
+        btnver2.Enabled = True
+        btnver3.Enabled = True
+        btnver4.Enabled = True
+        btnver5.Enabled = True
+        btnver6.Enabled = True
+        btnver7.Enabled = True
+        btnver8.Enabled = True
+        btnver9.Enabled = True
+        btnver10.Enabled = True
+        btnresposta.Enabled = True
+        btniniciar.Enabled = False
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub btnresposta_Click(sender As Object, e As EventArgs) Handles btnresposta.Click
+        lblcerto.Text = CStr(certo)
+        lblerrado.Text = CStr(errado)
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.F2 Then
+            btnfalso1.Enabled = True
+            btnfalso2.Enabled = True
+            btnfalso3.Enabled = True
+            btnfalso4.Enabled = True
+            btnfalso5.Enabled = True
+            btnfalso6.Enabled = True
+            btnfalso7.Enabled = True
+            btnfalso8.Enabled = True
+            btnfalso9.Enabled = True
+            btnfalso10.Enabled = True
+            btnver1.Enabled = True
+            btnver2.Enabled = True
+            btnver3.Enabled = True
+            btnver4.Enabled = True
+            btnver5.Enabled = True
+            btnver6.Enabled = True
+            btnver7.Enabled = True
+            btnver8.Enabled = True
+            btnver9.Enabled = True
+            btnver10.Enabled = True
+            btniniciar.Enabled = False
+        End If
+
+        If e.KeyCode = Keys.F3 Then btnresposta.Enabled = True
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Timer1.Interval = 5000 Then
+            MsgBox("O tempo acabou")
+            Me.Close()
+        End If
+
+    End Sub
+End Class
